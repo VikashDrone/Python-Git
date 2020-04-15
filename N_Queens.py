6709d879b6297708b9396d217fa6fd45824f7a61
sivikash <v.vikashsingh@gmail.com>
2020-04-16 00:38:50 +0200

    N_Queens 16 X 16 final code

--- N_Queens.py
+++ N_Queens.py
@@ -12,20 +12,20 @@ def not_in_row(matrix, row):
 
 def not_in_column(matrix, col):
     count = 0
-    for x in range(8):
+    for x in range(16):
         if matrix[x][col] == 1:
             count = count + 1
     return count
 
 
 def not_in_diagonal(matrix, row, col):
-    return np.sum(arr.diagonal(col - row)) + np.sum(np.fliplr(arr).diagonal(abs((col - 7)) - row))
+    return np.sum(arr.diagonal(col - row)) + np.sum(np.fliplr(arr).diagonal(abs((col - 15)) - row))
 
 
 def sum_matrix(matrix):
     sm = 0
-    for i in range(8):
-        for j in range(8):
+    for i in range(16):
+        for j in range(16):
             sm = sm + matrix[i][j]
     return sm
 
@@ -34,11 +34,11 @@ def generate_matrix(arr_tb, i, j, l_i, l_j, depth):
     print("Depth : "+str(depth))
     print(arr_tb)
     print("-----------------")
-    if sum_matrix(arr_tb) == 8:
+    if sum_matrix(arr_tb) == 16:
         return arr_tb
 
-    for i in range(8):
-        for j in range(8):
+    for i in range(16):
+        for j in range(16):
             if (not_in_row(arr_tb, i) + not_in_column(arr_tb, j) + not_in_diagonal(arr_tb, i, j)) == 0:
                 arr[i][j] = 1
                 arr_tb = generate_matrix(arr_tb, i + 1, 0, i, j, depth + 1)
@@ -48,8 +48,8 @@ def generate_matrix(arr_tb, i, j, l_i, l_j, depth):
     return arr_tb
 
 
-arr = np.zeros((8, 8))
-init_j = random.randint(0, 7)
+arr = np.zeros((16, 16))
+init_j = random.randint(0, 15)
 arr[0][init_j] = 1
 arr = generate_matrix(arr, 1, 0, 0, init_j, 0)
 print(arr)
